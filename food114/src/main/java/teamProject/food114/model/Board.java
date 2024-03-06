429e31911b855c8f991620f05c9901059710b4b6
gigigi <jj1421611@naver.com>
2024-03-06 18:26:25 +0900

    board

--- food114/src/main/java/teamProject/food114/model/Board.java
+++ food114/src/main/java/teamProject/food114/model/Board.java
@@ -4,5 +4,19 @@ import lombok.Data;
 
 @Data
 public class Board {
-
+	private int boardNo;
+	private String title;
+	private String contents;
+	private String beginTime;
+	private String endTime;
+	private String userId;
+	private int hit;
+	private int recommend;
+	private String type;
+	private String endYn;
+	private String cdateTime;
+	private String udateTime;
+	private int pboardNo;
+	private int code;
+	private int cnt;
 }
--- food114/src/main/java/teamProject/food114/service/BoardServiceImpl.java
+++ food114/src/main/java/teamProject/food114/service/BoardServiceImpl.java
@@ -1,7 +1,31 @@
 package teamProject.food114.service;
 
+import java.util.HashMap;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import teamProject.food114.mapper.BoardMapper;
+import teamProject.food114.model.Board;
 
 @Service
 public class BoardServiceImpl implements BoardService {
+	@Autowired
+	BoardMapper boardMapper;
+
+	@Override
+	public HashMap<String, Object> searchBoardList(HashMap<String, Object> map) {
+		// TODO Auto-generated method stub
+		HashMap<String, Object> resultMap = new HashMap<String, Object>();
+		List<Board> list = boardMapper.selectBoardList(map);
+		
+		// 댓글 개수
+		int cnt = boardMapper.selectBoardListCnt(map);
+		resultMap.put("list", list);
+		resultMap.put("cnt", cnt);
+		
+		resultMap.put("result", "success");
+		return resultMap;
+	}
 }
